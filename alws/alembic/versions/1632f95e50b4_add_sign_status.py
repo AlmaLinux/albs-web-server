2fc7d4ed7dae7b1e23384d43b6b074860b1da968
Alexander Komarov <akomarov.me@gmail.com>
2021-10-12 16:04:05 +0500

    Add missing alembic migrations

---
+++ alws/alembic/versions/1632f95e50b4_add_sign_status.py
@@ -0,0 +1,29 @@
+"""add_sign_status
+
+Revision ID: 1632f95e50b4
+Revises: 2fb5ffc7ff29
+Create Date: 2021-10-12 15:42:12.503594
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1632f95e50b4'
+down_revision = '2fb5ffc7ff29'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "build_tasks",
+        sa.Column(
+            "sign_status", sa.Integer(), nullable=False
+        ),
+    )
+
+
+def downgrade():
+    op.drop_column("build_tasks", "sign_status")
---
+++ alws/alembic/versions/2fb5ffc7ff29_add_pgp_key_id.py
@@ -0,0 +1,29 @@
+"""add_pgp_key_id
+
+Revision ID: 2fb5ffc7ff29
+Revises: 76eec955d203
+Create Date: 2021-10-12 15:33:10.230841
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2fb5ffc7ff29'
+down_revision = '76eec955d203'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "builds",
+        sa.Column(
+            "pgp_key_id", sa.Text(), nullable=False
+        ),
+    )
+
+
+def downgrade():
+    op.drop_column("builds", "pgp_key_id")
