1559edd7635019734ef4d76349329d0d49c92f58
Vasily Kleschov <vkleschov@cloudlinux.com>
2021-11-02 19:21:45 +0200

    Reverted changes for sync database session

--- alws/database.py
+++ alws/database.py
@@ -1,7 +1,7 @@
 # -*- mode:python; coding:utf-8; -*-
 # author: Vyacheslav Potoropin <vpotoropin@almalinux.org>
 # created: 2021-06-22
-
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
@@ -16,7 +16,9 @@ __all__ = ['Base', 'Session', 'engine']
 DATABASE_URL = settings.database_url
 
 engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
+sync_engine = create_engine(settings.sync_database_url)
 Base = declarative_base()
 Session = sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
 )
+SyncSession = sessionmaker(sync_engine, expire_on_commit=False)
