2ac118bb058973a88e50d100895591d59419e722
Daniil Anfimov <danfimov@cloudlinux.com>
2021-11-05 11:27:04 +0000

    ALBS-38: Added support saving/replacing debug noarch packages

--- alws/utils/noarch.py
+++ alws/utils/noarch.py
@@ -18,21 +18,25 @@ __all__ = [
 
 
 async def get_noarch_packages(
-        db: Session, build_task_ids: typing.List[int]) -> dict:
+    db: Session,
+    build_task_ids: typing.List[int]
+) -> typing.Tuple[dict, dict]:
     query = select(models.BuildTaskArtifact).where(sqlalchemy.and_(
         models.BuildTaskArtifact.build_task_id.in_(build_task_ids),
         models.BuildTaskArtifact.type == 'rpm',
         models.BuildTaskArtifact.name.like('%.noarch.%'),
-        models.BuildTaskArtifact.name.not_like('%-debuginfo-%'),
-        models.BuildTaskArtifact.name.not_like('%-debugsource-%'),
     ))
     db_artifacts = await db.execute(query)
     db_artifacts = db_artifacts.scalars().all()
-    noarch_packages = {
-        artifact.name: artifact.href
-        for artifact in db_artifacts
-    }
-    return noarch_packages
+    noarch_packages = {}
+    debug_noarch_packages = {}
+    for artifact in db_artifacts:
+        if '-debuginfo-' in artifact.name or '-debugsource-' in artifact.name:
+            debug_noarch_packages[artifact.name] = artifact.href
+            continue
+        noarch_packages[artifact.name] = artifact.href
+
+    return noarch_packages, debug_noarch_packages
 
 
 async def save_noarch_packages(db: Session, build_task: models.BuildTask):
@@ -57,20 +61,24 @@ async def save_noarch_packages(db: Session, build_task: models.BuildTask):
             return
 
         build_task_ids = [task.id for task in build_tasks]
-        noarch_packages = await get_noarch_packages(db, build_task_ids)
-        if not noarch_packages:
+        noarch_packages, debug_noarch_packages = await get_noarch_packages(
+            db, build_task_ids)
+        if not any((noarch_packages, debug_noarch_packages)):
             return
 
         repos_to_update = {}
         new_noarch_artifacts = []
         hrefs_to_add = list(noarch_packages.values())
+        debug_hrefs_to_add = list(debug_noarch_packages.values())
 
         for task in build_tasks:
             if task.status in (BuildTaskStatus.FAILED,
                                BuildTaskStatus.EXCLUDED):
                 continue
             noarch = copy.deepcopy(noarch_packages)
+            debug_noarch = copy.deepcopy(debug_noarch_packages)
             hrefs_to_delete = []
+            debug_hrefs_to_delete = []
 
             # replace hrefs for existing artifacts in database
             # and create new artifacts if they doesn't exist
@@ -78,7 +86,12 @@ async def save_noarch_packages(db: Session, build_task: models.BuildTask):
                 if artifact.name in noarch:
                     hrefs_to_delete.append(artifact.href)
                     artifact.href = noarch.pop(artifact.name)
-            for name, href in noarch.items():
+                if artifact.name in debug_noarch:
+                    debug_hrefs_to_delete.append(artifact.href)
+                    artifact.href = debug_noarch.pop(artifact.name)
+
+            artifacts_to_create = {**noarch, **debug_noarch}
+            for name, href in artifacts_to_create.items():
                 new_noarch_artifacts.append(
                     models.BuildTaskArtifact(
                         build_task_id=task.id,
@@ -87,16 +100,20 @@ async def save_noarch_packages(db: Session, build_task: models.BuildTask):
                         href=href,
                     )
                 )
-            repo_href = next(
-                repo.pulp_href for repo in build_task.build.repos
-                if repo.arch == task.arch
-                and repo.type == 'rpm'
-                and repo.debug is False
-            )
-            repos_to_update[repo_href] = {
-                'add': hrefs_to_add,
-                'remove': hrefs_to_delete,
-            }
+
+            for repo in build_task.build.repos:
+                if repo.arch != task.arch and repo.type != 'rpm':
+                    continue
+                repo_href = repo.pulp_href
+                add_content = hrefs_to_add
+                remove_content = hrefs_to_delete
+                if repo.debug:
+                    add_content = debug_hrefs_to_add
+                    remove_content = debug_hrefs_to_delete
+                repos_to_update[repo_href] = {
+                    'add': add_content,
+                    'remove': remove_content,
+                }
 
         db.add_all(build_tasks)
         db.add_all(new_noarch_artifacts)
