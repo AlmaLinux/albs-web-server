3431a1bb5d97f318e2b0c35a90220281f37a168d
Ruslan Pisarev <rpisarev@cloudlinux.com>
2022-01-17 09:06:53 +0200

    Changed type export to hardlink and removed debug output

--- alws/utils/pulp_client.py
+++ alws/utils/pulp_client.py
@@ -328,7 +328,7 @@ class PulpClient:
     async def update_filesystem_exporter(self, fse_pulp_href: str,
                                          fse_name: str,
                                          fse_path: str,
-                                         fse_method: str='symlink'):
+                                         fse_method: str='hardlink'):
         endpoint = fse_pulp_href
         params = {
             'name': fse_name,
