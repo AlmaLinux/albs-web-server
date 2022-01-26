0f5856260d44b36785c2b8c9219ffd15296050a7
Ruslan Pisarev <rpisarev@cloudlinux.com>
2022-01-26 12:36:50 +0200

    Fixed signing for DB.

--- alws/models.py
+++ alws/models.py
@@ -104,6 +104,7 @@ class Platform(Base):
     arch_list = sqlalchemy.Column(JSONB, nullable=False)
     data = sqlalchemy.Column(JSONB, nullable=False)
     repos = relationship('Repository', secondary=PlatformRepo)
+    sign_keys = relationship('SignKey', back_populates='platform')
 
 
 class Distribution(Base):
@@ -339,6 +340,13 @@ class BuildTaskArtifact(Base):
     type = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
     href = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
     build_task = relationship('BuildTask', back_populates='artifacts')
+    sign_key_id = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        sqlalchemy.ForeignKey('sign_keys.id',
+                              name='build_artifacts_sign_key_id_fkey'),
+        nullable=True)
+    sign_key = relationship('SignKey',
+                            back_populates='build_task_artifacts')
 
 
 class SourceRpm(Base):
@@ -358,13 +366,6 @@ class SourceRpm(Base):
     )
     artifact = relationship('BuildTaskArtifact')
     binary_rpms = relationship('BinaryRpm', back_populates='source_rpm')
-    signed_by_key_id = sqlalchemy.Column(
-        sqlalchemy.Integer,
-        sqlalchemy.ForeignKey('sign_keys.id',
-                              name='source_rpms_signed_by_key_id_fkey'),
-        nullable=True
-    )
-    signed_by_key = relationship('SignKey')
 
 
 class BinaryRpm(Base):
@@ -389,13 +390,6 @@ class BinaryRpm(Base):
         nullable=False
     )
     source_rpm = relationship('SourceRpm', back_populates='binary_rpms')
-    signed_by_key_id = sqlalchemy.Column(
-        sqlalchemy.Integer,
-        sqlalchemy.ForeignKey('sign_keys.id',
-                              name='binary_rpms_signed_by_key_id_fkey'),
-        nullable=True
-    )
-    signed_by_key = relationship('SignKey')
 
 
 class User(Base):
@@ -492,6 +486,14 @@ class SignKey(Base):
     public_url = sqlalchemy.Column(sqlalchemy.Text)
     inserted = sqlalchemy.Column(
         sqlalchemy.DateTime, default=datetime.datetime.utcnow())
+    platform_id = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        sqlalchemy.ForeignKey('platforms.id',
+                              name='sign_keys_platform_id_fkey'),
+        nullable=True)
+    platform = relationship('Platform', back_populates='sign_keys')
+    build_task_artifacts = relationship('BuildTaskArtifact',
+                                        back_populates='sign_key')
 
 
 class SignTask(Base):
