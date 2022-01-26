f5baa58aa0503103bcd1154ac506546c01518242
Ruslan Pisarev <rpisarev@cloudlinux.com>
2022-01-26 13:49:02 +0200

    Resolve alembic heads conflict.

--- alws/alembic/versions/936845bf5a4f_adding_fkeys_for_build_artifacts_and_.py
+++ alws/alembic/versions/936845bf5a4f_adding_fkeys_for_build_artifacts_and_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '936845bf5a4f'
-down_revision = 'aa0a3bdf68d4'
+down_revision = '2e1d89155486'
 branch_labels = None
 depends_on = None
 
