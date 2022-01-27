e379d300b969bebedb30801eff8bfb8df4e72ebb
Ruslan Pisarev <rpisarev@cloudlinux.com>
2022-01-27 11:51:00 +0200

    SignError exception added to catch flow and it will be able to show in UI in the future.

--- alws/crud/release.py
+++ alws/crud/release.py
@@ -163,12 +163,16 @@ async def execute_release_plan(release_id: int, db: Session):
             raise EmptyReleasePlan('Cannot execute plan with empty packages '
                                    'or repositories: {packages}, {repositories}'
                                    .format_map(release.plan))
-        verified = True
         for build_id in release.build_ids:
-            verified = verified and sign_task.verify_signed_build(
-                db, build_id, release.platform_id)
-        if not verified:
-            raise SignError('Cannot execute plan with wrong singing of pkg')
+            try:
+                verified = sign_task.verify_signed_build(
+                    db, build_id, release.platform_id)
+            except (DataNotFoundError, ValueError) as e:
+                msg = f'The build {build_id} was not verified, because\n{e}'
+                raise SignError(msg)
+            if not verified:
+                msg = f'Cannot execute plan with wrong singing of {build_id}'
+                raise SignError(msg)
     for package in release.plan['packages']:
         for repository in package['repositories']:
             repo_name = repository['name']
@@ -319,7 +323,7 @@ async def commit_release(db: Session, release_id: int) -> (models.Release, str):
         await db.commit()
     try:
         await execute_release_plan(release_id, db)
-    except (EmptyReleasePlan, MissingRepository) as e:
+    except (EmptyReleasePlan, MissingRepository, SignError) as e:
         message = f'Cannot commit release: {str(e)}'
         release.status = ReleaseStatus.FAILED
     else:
