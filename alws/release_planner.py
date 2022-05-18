a9cd868cef21fca074ad67392f124b20c6051d0d
Daniil Anfimov <anfimovdan@gmail.com>
2022-05-19 00:36:18 +0200

    added new params for beholder request

--- alws/release_planner.py
+++ alws/release_planner.py
@@ -510,7 +510,7 @@ class ReleasePlanner:
 
         beholder_responses = await self._beholder_client.retrieve_responses(
             base_platform,
-            data=src_rpm_names,
+            data={'source_rpms': src_rpm_names, 'match': 'closest'},
         )
         for beholder_response in beholder_responses:
             distr = beholder_response['distribution']
