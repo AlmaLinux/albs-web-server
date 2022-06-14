7542722ba5db54176e5405a8cc380b805541f6ce
Vasiliy Kleschov <kleshev12@gmail.com>
2022-06-14 18:00:06 +0300

    Moved completing test tasks into dramatiq queue (#221)
    * Moved completing test tasks into dramatiq queue
    
    * Fixed code review comments
    
    * Fixed wrongly set test statuses
    
    * Removed unused import
    
    Co-authored-by: Vasily Kleschov <vkleschov@cloudlinux.com>

--- alws/routers/tests.py
+++ alws/routers/tests.py
@@ -1,8 +1,8 @@
 import typing
 
-from fastapi import APIRouter, BackgroundTasks, Depends
+from fastapi import APIRouter, Depends
 
-from alws import database
+from alws import database, dramatiq
 from alws.crud import test
 from alws.dependencies import get_db, JWTBearer
 from alws.schemas import test_schema
@@ -22,11 +22,8 @@ public_router = APIRouter(
 
 @router.post('/{test_task_id}/result/')
 async def update_test_task_result(test_task_id: int,
-                                  result: test_schema.TestTaskResult,
-                                  background_tasks: BackgroundTasks,
-                                  db: database.Session = Depends(get_db)):
-    background_tasks.add_task(
-        test.complete_test_task, db, test_task_id, result)
+                                  result: test_schema.TestTaskResult):
+    dramatiq.tests.complete_test_task.send(test_task_id, result.dict())
     return {'ok': True}
 
 
