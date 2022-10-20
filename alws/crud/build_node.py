c808a919d6235bb77e6d791e337327c99b4ade50
Daniil Anfimov <anfimovdan@gmail.com>
2022-10-21 01:14:14 +0200

    Added parallel restart failed built items

--- alws/crud/build_node.py
+++ alws/crud/build_node.py
@@ -3,6 +3,7 @@ import datetime
 import logging
 import typing
 import traceback
+from collections import defaultdict
 
 import sqlalchemy
 from sqlalchemy import delete, insert, update
@@ -78,15 +79,66 @@ def add_build_task_dependencies(db: Session, task: models.BuildTask,
     task.dependencies.append(last_task)
 
 
+def generate_restart_tasks_query(build_id: int):
+    query = (
+        select(models.BuildTask)
+        .where(
+            sqlalchemy.and_(
+                models.BuildTask.build_id == build_id,
+                models.BuildTask.status == BuildTaskStatus.FAILED
+            ),
+        )
+        .order_by(models.BuildTask.index, models.BuildTask.id)
+    )
+    return query
+
+
+async def update_failed_build_items_in_parallel(db: Session, build_id: int):
+    async with db.begin():
+        failed_tasks = await db.execute(generate_restart_tasks_query(build_id))
+        tasks_cache = {}
+        for task in failed_tasks.scalars().all():
+            idx = task.index
+            platform_id = task.platform_id
+            arch = task.arch
+            cache_key = (platform_id, arch)
+            if idx not in tasks_cache:
+                tasks_cache[idx] = defaultdict(dict)
+            tasks_cache[idx][cache_key] = task
+        tasks_indexes = list(tasks_cache.keys())
+        for task_index, index_dict in tasks_cache.items():
+            current_idx = tasks_indexes.index(task_index)
+            first_index_dep = None
+            for key in sorted(
+                list(index_dict.keys()),
+                key=lambda x: x[1] == "i686",
+                reverse=True,
+            ):
+                task = index_dict[key]
+                task.status = BuildTaskStatus.IDLE
+                task.ts = None
+                if first_index_dep:
+                    await db.run_sync(
+                        add_build_task_dependencies, task, first_index_dep
+                    )
+                idx = current_idx - 1
+                while idx >= 0:
+                    prev_task_index = tasks_indexes[idx]
+                    dep = tasks_cache.get(prev_task_index, {}).get(key)
+                    if dep:
+                        await db.run_sync(
+                            add_build_task_dependencies, task, dep
+                        )
+                    idx -= 1
+                if first_index_dep is None:
+                    first_index_dep = task
+        await db.commit()
+
+
 async def update_failed_build_items(db: Session, build_id: int):
-    query = select(models.BuildTask).where(
-        sqlalchemy.and_(
-            models.BuildTask.build_id == build_id,
-            models.BuildTask.status == BuildTaskStatus.FAILED)
-    ).order_by(models.BuildTask.index, models.BuildTask.id)
     async with db.begin():
         last_task = None
-        failed_tasks = await db.execute(query)
+        failed_tasks = await db.execute(generate_restart_tasks_query(build_id))
         for task in failed_tasks.scalars():
             task.status = BuildTaskStatus.IDLE
             task.ts = None
