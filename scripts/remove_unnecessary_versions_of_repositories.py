973a07d652930caddf8236d26f2ac99ad8252e5e
maccelf <maxmol27@gmail.com>
2023-01-16 07:43:39 +0100

    ALBS-919 Decrease Pulp storage size for backup

---
+++ scripts/remove_unnecessary_versions_of_repositories.py
@@ -0,0 +1,181 @@
+# Script: remove_unnecessery_versions_of_repositories.py
+# Author:
+# - Maxim Petukhov <mpetukhov@cloudlinux.com>
+#
+# Date: 13 Jan 2023
+#
+# Description: This script remove unnecessery versions of repositories in pulp:
+# - Delete no more than 3 versions of production repositories
+# - Delete no more than 1 versions of build repositories
+#
+# Usage: This script requires direct access to both Pulp
+# and Build System DBs. For configuration issues ask in
+# buildsys-internal channel on CloudLinux Slack.
+# This script should be placed inside albs-web-server
+# scripts folder as it uses modules inside the repo.
+# AFTER running the script PLEASE run "orphans/cleanup" in Pulp
+# to remove all orpgans artifacts.
+# REMEMBER: Do Pulp storoge backup before running the script in
+# production just in case anything goes wrong.
+#
+import asyncio
+import os
+import sys
+import argparse
+import typing
+import re
+import logging
+import urllib.parse
+
+from sqlalchemy.future import select
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from alws import database
+from alws import models
+from alws.utils.pulp_client import PulpClient
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        "pulp_artifacts_remover",
+        description="Remove unnecessery versions of repositories in Pulp",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        default=False,
+        required=False,
+        help="Enable verbose output",
+    )
+    return parser.parse_args()
+
+
+# Remove all orphans from Pulp
+# In production we have broken repository and becouse of that
+# this function is return error.
+async def remove_orphans(
+    pulp_client: PulpClient,
+    logger: logging.Logger,
+):
+    endpoint = "pulp/api/v3/orphans/cleanup/"
+    logger.info("Start task to remove orphans")
+    result = await pulp_client.request("POST", endpoint)
+    logger.debug("Task to remove orphans is created: %s", result["task"])
+    logger.info("Waiting for deletion of orphans...")
+    task = await pulp_client.wait_for_task(result["task"])
+    logger.info("All orphans is removed")
+    return task
+
+
+# Change retain_repo_versions to min value and return it back
+# After changing the retain_repo_versions Pulp will remove unrelated versions
+async def remove_unnecessery_versions(
+    pulp_client: PulpClient,
+    min_retain_repo_versions: int,
+    pulp_repo: dict,
+    logger: logging.Logger,
+):
+    endpoint = pulp_repo["pulp_href"]
+    params = {"retain_repo_versions": min_retain_repo_versions}
+    result = await pulp_client.request("PATCH", endpoint, json=params)
+    logger.debug(
+        "Create task to change retain repo versions to %s",
+        params["retain_repo_versions"],
+    )
+    logger.debug(result)
+    params = {"retain_repo_versions": pulp_repo["retain_repo_versions"]}
+    result = await pulp_client.request("PATCH", endpoint, json=params)
+    logger.debug(
+        "Create task to back retain repo versions to %s",
+        params["retain_repo_versions"]
+    )
+    logger.debug(result)
+
+
+async def get_all_pulp_repositories(
+    pulp_client: PulpClient,
+    logger: logging.Logger,
+):
+    endpoint = "pulp/api/v3/repositories/rpm/rpm/"
+    params = {"fields": "id,pulp_href,retain_repo_versions"}
+    result = []
+
+    def update_result(response: typing.List[dict]):
+        for rec in response:
+            result.append(rec)
+
+    response = await pulp_client.request("GET", endpoint, params=params)
+    update_result(response.get("results", []))
+    next_page = response.get("next")
+    if not next_page:
+        return result
+    while True:
+        if (
+            "limit" in next_page
+            and re.search(r"limit=(\d+)", next_page).groups()[0] == "100"
+        ):
+            next_page = next_page.replace("limit=100", "limit=1000")
+        parsed_url = urllib.parse.urlsplit(next_page)
+        path = parsed_url.path + "?" + parsed_url.query
+        response = await pulp_client.get_by_href(path)
+        update_result(response.get("results", []))
+        next_page = response.get("next")
+        if not next_page:
+            break
+    logger.debug("%s repositories received from Pulp", len(result))
+    return result
+
+
+async def get_production_repositories(
+    pulp_client: PulpClient,
+    logger: logging.Logger,
+):
+    async with database.Session() as db:
+        repo_q = select(models.Repository).where(
+            models.Repository.production == True
+        )
+        repositories = await db.execute(repo_q)
+    result = repositories.scalars().all()
+    logger.debug("%s production repositories received from DB", len(result))
+    return result
+
+
+async def main():
+    pulp_host = os.environ["PULP_HOST"]
+    pulp_user = os.environ["PULP_USER"]
+    pulp_password = os.environ["PULP_PASSWORD"]
+    args = parse_args()
+    logger = logging.getLogger("pulp_artifacts_remover")
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+    pulp_client = PulpClient(pulp_host, pulp_user, pulp_password)
+
+    logger.info("Get all pulp repos")
+    pulp_repos = await get_all_pulp_repositories(pulp_client, logger)
+    logger.info("Get all production repos")
+    prod_repos = await get_production_repositories(pulp_client, logger)
+    prod_repos_href = []
+    for prod_repo in prod_repos:
+        prod_repos_href.append(prod_repo.pulp_href)
+    logger.info("Removing unnecessery versions of repositories...")
+    for pulp_repo in pulp_repos:
+        if pulp_repo["pulp_href"] in prod_repos_href:
+            retain_repo_versions = 3
+        else:
+            retain_repo_versions = 1
+        await remove_unnecessery_versions(
+            pulp_client=pulp_client,
+            min_retain_repo_versions=retain_repo_versions,
+            pulp_repo=pulp_repo,
+            logger=logger,
+        )
+    logger.info("All unnecessery version of repositories is deleted")
+    # await remove_orphans(pulp_client, logger)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
