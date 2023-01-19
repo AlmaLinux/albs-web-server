99863284fbf3cd748b57e3cca7fcf20140bf219d
maccelf <maxmol27@gmail.com>
2023-01-19 15:32:19 +0100

    ALBS-669 Add a meaningful message error when signing builds

--- alws/middlewares/__init__.py
+++ alws/middlewares/__init__.py
@@ -1,6 +1,8 @@
 from .permissions import handlers as perm_handlers
 from .products import handlers as product_handlers
 from .uploads import handlers as upload_handlers
+from .not_found import handlers as not_found_handler
+from .sign_task import handlers as sign_task_handlers
 
 
 __all__ = ['handlers']
@@ -10,6 +12,8 @@ add_handlers = (
     perm_handlers,
     product_handlers,
     upload_handlers,
+    not_found_handler,
+    sign_task_handlers,
 )
 handlers = {}
 for add_handler in add_handlers:
