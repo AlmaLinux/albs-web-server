f5b57d7a9223efe5828981da35572ea75cbc02ec
maccelf <maxmol27@gmail.com>
2023-03-30 18:17:03 +0200

    ALBS-1065: Remove file signing functionality from web server code

--- alws/routers/sign_task.py
+++ alws/routers/sign_task.py
@@ -1,19 +1,16 @@
 import uuid
-import json
 import typing
 import datetime
 
-import aioredis
 from fastapi import (
     APIRouter,
     Depends,
-    WebSocket,
 )
 
 from alws import database
 from alws.auth import get_current_user
 from alws.crud import sign_task
-from alws.dependencies import get_db, get_redis
+from alws.dependencies import get_db
 from alws import dramatiq
 from alws.schemas import sign_schema
 
@@ -66,56 +63,3 @@ async def complete_sign_task(
     await db.commit()
     dramatiq.sign_task.complete_sign_task.send(sign_task_id, payload.dict())
     return {'success': True}
-
-
-@router.post(
-    '/sync_sign_task/',
-    response_model=typing.Union[
-        sign_schema.SyncSignTaskResponse,
-        sign_schema.SyncSignTaskError,
-    ],
-)
-async def create_small_sign_task(
-            payload: sign_schema.SyncSignTaskRequest,
-            redis: aioredis.Redis = Depends(get_redis)
-        ):
-    task_id = str(uuid.uuid1())
-    task_payload = {
-        'task_id': task_id,
-        'content': payload.content,
-        'key_id': payload.pgp_keyid,
-        'sig_type': payload.sig_type,
-    }
-    pubsub = redis.pubsub()
-    await pubsub.subscribe(task_id)
-    await redis.publish('small_sign_tasks', json.dumps(task_payload))
-    while True:
-        message = await pubsub.get_message(
-            ignore_subscribe_messages=True,
-            timeout=60
-        )
-        if not message:
-            continue
-        # First message arrived at this channel is our answer
-        return json.loads(message['data'])
-
-
-@router.websocket('/sign_task_queue/')
-async def iter_sync_sign_tasks(
-            websocket: WebSocket,
-            redis: aioredis.Redis = Depends(get_redis)
-        ):
-    await websocket.accept()
-    pubsub = redis.pubsub()
-    await pubsub.subscribe('small_sign_tasks')
-    while True:
-        message = await pubsub.get_message(
-            ignore_subscribe_messages=True,
-            timeout=60
-        )
-        if message is None:
-            continue
-        payload = json.loads(message['data'])
-        await websocket.send_text(message['data'].decode())
-        response = await websocket.receive_text()
-        await redis.publish(payload['task_id'], response)
