6b1cddfe589c96cb56996bafc4e1924e4692c652
Javier Hernández <jhernandez@cloudlinux.com>
2023-04-26 11:27:25 +0200

    ALBS-1104: Linted dramatiq/products.py

--- alws/dramatiq/products.py
+++ alws/dramatiq/products.py
@@ -24,7 +24,7 @@ async def get_existing_packages(
     repository: models.Repository,
 ) -> typing.List[typing.Dict[str, str]]:
 
-    pulp_fields = ['pulp_href', 'artifact', 'sha256', 'location_href', 'arch']
+    pulp_fields = ["pulp_href", "artifact", "sha256", "location_href", "arch"]
     return await pulp_client.get_rpm_repository_packages(
         repository.pulp_href,
         include_fields=pulp_fields,
@@ -42,42 +42,44 @@ async def get_packages(
     def filter_by_arch(pkgs: typing.List[dict], repo_arch: str):
         filtered = []
         for pkg in pkgs:
-            if pkg['arch'] == 'noarch' and repo_arch != 'src':
+            if pkg["arch"] == "noarch" and repo_arch != "src":
                 filtered.append(pkg)
-            elif pkg['arch'] == 'i686' and repo_arch in ('i686', 'x86_64'):
+            elif pkg["arch"] == "i686" and repo_arch in ("i686", "x86_64"):
                 filtered.append(pkg)
-            elif pkg['arch'] == repo_arch:
+            elif pkg["arch"] == repo_arch:
                 filtered.append(pkg)
         return filtered
 
     dist_packages = await get_existing_packages(pulp_client, dist_repo)
-    search_by_href = set([pkg['pulp_href'] for pkg in dist_packages])
+    search_by_href = set([pkg["pulp_href"] for pkg in dist_packages])
     build_packages = await get_existing_packages(pulp_client, build_repo)
     filtered_build_packages = filter_by_arch(build_packages, dist_repo.arch)
-    logger.debug('Packages in product repository %s:\n%s', dist_repo.name,
+    logger.debug("Packages in product repository %s:\n%s", dist_repo.name,
                  pprint.pformat(dist_packages))
-    logger.debug('Set of packages HREFs for comparison '
-                 'with build artifacts:\n%s',
+    logger.debug("Set of packages HREFs for comparison "
+                 "with build artifacts:\n%s",
                  pprint.pformat(search_by_href))
-    logger.debug('List of build packages in build repository %s:\n%s',
+    logger.debug("List of build packages in build repository %s:\n%s",
                  build_repo.name, pprint.pformat(filtered_build_packages))
-    if modification == 'add':
+    if modification == "add":
         dedup_mapping = {}
         for pkg in filtered_build_packages:
-            if pkg['location_href'] in dedup_mapping:
+            if pkg["location_href"] in dedup_mapping:
                 continue
-            dedup_mapping[pkg['location_href']] = pkg['pulp_href']
-        logger.debug('Deduplication mapping for packages'
-                     'with the same name:\n%s', pprint.pformat(dedup_mapping))
+            dedup_mapping[pkg["location_href"]] = pkg["pulp_href"]
+        logger.debug("Deduplication mapping for packages"
+                     "with the same name:\n%s", pprint.pformat(dedup_mapping))
         final_packages = [
             href for href in dedup_mapping.values()
             if href not in search_by_href
-            and pkg['pulp_href'] not in pkgs_blacklist
+            and pkg["pulp_href"] not in pkgs_blacklist
         ]
     else:
-        final_packages = [pkg['pulp_href'] for pkg in filtered_build_packages
-                          if pkg['pulp_href'] in search_by_href]
-    logger.debug('Final list of packages to %s:\n%s', modification,
+        final_packages = [
+            pkg["pulp_href"] for pkg in filtered_build_packages
+            if pkg["pulp_href"] in search_by_href
+        ]
+    logger.debug("Final list of packages to %s:\n%s", modification,
                  pprint.pformat(final_packages))
     return dist_repo.pulp_href, final_packages
 
@@ -93,7 +95,7 @@ async def prepare_repo_modify_dict(
     product_repo_mapping = {(repo.arch, repo.debug, repo.platform.name): repo
                             for repo in db_product.repositories}
     modify = defaultdict(list)
-    build_repos = [repo for repo in db_build.repos if repo.type == 'rpm']
+    build_repos = [repo for repo in db_build.repos if repo.type == "rpm"]
     tasks = []
     for repo in build_repos:
         dist_repo = product_repo_mapping.get(
@@ -123,12 +125,12 @@ async def prepare_repo_modify_dict(
 
 
 async def set_platform_for_products_repos(
-        db: Session,
-        product: models.Product,
+    db: Session,
+    product: models.Product,
 ) -> None:
     repo_debug_dict = {
-        True: 'debug-dr',
-        False: 'dr',
+        True: "debug-dr",
+        False: "dr",
     }
     # name of a product's repo:
     # some-username-some-product-some-platform-x86_64-debug-dr
@@ -145,12 +147,12 @@ async def set_platform_for_products_repos(
 
 
 async def set_platform_for_build_repos(
-        db: Session,
-        build: models.Build,
+    db: Session,
+    build: models.Build,
 ) -> None:
     repo_debug_dict = {
-        True: 'debug-br',
-        False: 'br',
+        True: "debug-br",
+        False: "br",
     }
     # name of a build's repo:
     # some-platform-x86_64-some-build-id-debug-br
@@ -163,15 +165,16 @@ async def set_platform_for_build_repos(
     if all(repo.platform for repo in build.repos):
         return
     for repo in build.repos:
-        if repo.type != 'rpm':
+        if repo.type != "rpm":
             continue
         repo.platform = repos_per_platform[repo.name]
 
 
 async def _perform_product_modification(
-        build_id: int,
-        product_id: int,
-        modification: str):
+    build_id: int,
+    product_id: int,
+    modification: str,
+):
 
     pulp_client = PulpClient(settings.pulp_host, settings.pulp_user,
                              settings.pulp_password)
@@ -259,7 +262,7 @@ async def _perform_product_modification(
     tasks = []
     publish_tasks = []
     for key, value in modify.items():
-        if modification == 'add':
+        if modification == "add":
             tasks.append(pulp_client.modify_repository(add=value, repo_to=key))
         else:
             tasks.append(pulp_client.modify_repository(
@@ -271,7 +274,7 @@ async def _perform_product_modification(
     await asyncio.gather(*tasks)
     await asyncio.gather(*publish_tasks)
 
-    if modification == 'add':
+    if modification == "add":
         db_product.builds.append(db_build)
     else:
         db_product.builds.remove(db_build)
@@ -285,11 +288,17 @@ async def _perform_product_modification(
         await db.rollback()
 
 
-@dramatiq.actor(max_retries=0, priority=0, queue_name='product_modify', time_limit=DRAMATIQ_TASK_TIMEOUT)
+@dramatiq.actor(
+    max_retries=0,
+    priority=0,
+    queue_name="product_modify",
+    time_limit=DRAMATIQ_TASK_TIMEOUT
+)
 def perform_product_modification(
-        build_id: int,
-        product_id: int,
-        modification: str):
+    build_id: int,
+    product_id: int,
+    modification: str,
+):
     event_loop.run_until_complete(_perform_product_modification(
         build_id, product_id, modification
     ))
