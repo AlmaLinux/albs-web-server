627e0c41fe4c3e758c6c389d393df4ab4c08f25d
Javier Hernández <jhernandez@cloudlinux.com>
2023-04-28 10:54:16 +0200

    ALBS-1104: Extended products api tests

--- tests/test_api/test_products.py
+++ tests/test_api/test_products.py
@@ -1,8 +1,13 @@
 import pytest
 
-from alws.models import Product
-from tests.mock_classes import BaseAsyncTestCase
+from alws.models import Product, Build
+from alws.dramatiq.products import _perform_product_modification
 
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from tests.mock_classes import BaseAsyncTestCase
 
 @pytest.mark.usefixtures(
     "base_platform",
@@ -36,10 +41,12 @@ class TestProductsEndpoints(BaseAsyncTestCase):
 
     async def test_add_to_product(
         self,
-        regular_build,
-        product_create_payload,
+        regular_build: Build,
+        base_product: Product,
+        session: AsyncSession
     ):
-        product_name = product_create_payload['name']
+        product_id = base_product.id
+        product_name = base_product.name
         build_id = regular_build.id
         endpoint = f"/api/v1/products/add/{build_id}/{product_name}/"
         response = await self.make_request(
@@ -49,3 +56,48 @@ class TestProductsEndpoints(BaseAsyncTestCase):
 
         message = f"Cannot add build to product:\n{response.text}"
         assert response.status_code == self.status_codes.HTTP_200_OK, message
+
+        # dramatic.Actor.send is monkeypatched to return None.
+        # That's why we manually call _perform_product_modification here.
+        # In case there's an error in add_to_product, it will be raised and
+        # the test will be reported as failed.
+        await _perform_product_modification(build_id, product_id, "add")
+        db_product = (await session.execute(
+            select(Product).where(
+                Product.id == product_id
+            ).options(
+                selectinload(Product.builds)
+            )
+        )).scalars().first()
+
+        assert db_product.builds[0].id == build_id, message
+
+
+    async def test_remove_from_product(
+        self,
+        base_product: Product,
+        session: AsyncSession
+    ):
+        product_id = base_product.id
+        product_name = base_product.name
+        # We remove the build created in the previous test
+        build_id = 1
+        endpoint = f"/api/v1/products/remove/{build_id}/{product_name}/"
+        response = await self.make_request(
+            "post",
+            endpoint
+        )
+
+        message = f"Cannot remove build from product:\n{response.text}"
+        assert response.status_code == self.status_codes.HTTP_200_OK, message
+        await _perform_product_modification(build_id, product_id, "remove")
+        db_product = (await session.execute(
+            select(Product).where(
+                Product.id == product_id
+            ).options(
+                selectinload(Product.builds)
+            )
+        )).scalars().first()
+
+        # At this point, db_product shouldn't have any build
+        assert not db_product.builds, message
