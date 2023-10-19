3603c41fefd6dbe7fd11d1c043b8c921ca0f2548
Vasily Kleschov <kleshev12@gmail.com>
2023-10-19 10:32:57 +0200

    Fixed code styling

--- tests/test_api/test_sign_keys.py
+++ tests/test_api/test_sign_keys.py
@@ -2,7 +2,9 @@ from tests.mock_classes import BaseAsyncTestCase
 
 
 class TestSignKeys(BaseAsyncTestCase):
-    async def test_create_new_key_incorrect_platform(self, basic_sign_key_payload):
+    async def test_create_new_key_incorrect_platform(
+        self, basic_sign_key_payload
+    ):
         payload = basic_sign_key_payload.copy()
         payload["platform_id"] = 9999
         response = await self.make_request(
@@ -12,9 +14,7 @@ class TestSignKeys(BaseAsyncTestCase):
         assert response.json()["detail"].startswith('No platform with id')
 
     async def test_create_new_key_already_exists(
-        self,
-        sign_key,
-        basic_sign_key_payload
+        self, sign_key, basic_sign_key_payload
     ):
         response = await self.make_request(
             "post", f"/api/v1/sign-keys/new/", json=basic_sign_key_payload
@@ -24,9 +24,7 @@ class TestSignKeys(BaseAsyncTestCase):
         assert response.json()["detail"] == err_msg
 
     async def test_create_new_key_with_platform(
-        self,
-        base_platform,
-        basic_sign_key_payload
+        self, base_platform, basic_sign_key_payload
     ):
         payload = basic_sign_key_payload.copy()
         payload["platform_id"] = base_platform.id
