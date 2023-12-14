2a92f5d8793db5eb0893c34c538a8191ff911618
Kirill Zhukov <kzhukov@cloudlinux.com>
2023-12-14 16:24:40 +0000

    Added test for packages_exporter`s new logic Fixed some bugs

---
+++ tests/test_scripts/test_packages_exporter.py
@@ -0,0 +1,16 @@
+import tempfile
+
+from scripts.packages_exporter import Exporter
+from syncer import sync
+
+
+def test_repomd_signer():
+    exporter = Exporter(pulp_client=None, 
+                        repodata_cache_dir='~/.cache/pulp_exporter')
+    
+    db_keys = sync(exporter.get_sign_keys())
+    key_id = db_keys[0]['keyid']
+    with tempfile.NamedTemporaryFile() as fp:
+        fp.write(b'Hello world!')
+        res = sync(exporter.sign_repomd_xml(fp.name, key_id))
+        assert res['error'] is None
