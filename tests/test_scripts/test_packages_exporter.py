a3005f6c30553f5fa8007686d531c75934b2d255
Kirill Zhukov <kzhukov@cloudlinux.com>
2023-12-19 09:31:25 +0000

    Disabled test test_packages_exporter.py from running in Github Actions enviroment

--- tests/test_scripts/test_packages_exporter.py
+++ tests/test_scripts/test_packages_exporter.py
@@ -1,15 +1,17 @@
-import tempfile
-
+import os
+import pytest
 from syncer import sync
+import tempfile
 
 from scripts.packages_exporter import Exporter
 
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 def test_repomd_signer():
     exporter = Exporter(
         pulp_client=None, repodata_cache_dir='~/.cache/pulp_exporter'
     )
-
     db_keys = sync(exporter.get_sign_keys())
     key_id = db_keys[0]['keyid']
     with tempfile.NamedTemporaryFile() as fp:
