4137110d1f074271fa8cc1611fa555d24d96326b
Javier Hernández <jhernandez@cloudlinux.com>
2024-02-27 15:44:21 +0100

    Skip packages_exporter test
    It needs to be refactored.

--- tests/test_scripts/test_packages_exporter.py
+++ tests/test_scripts/test_packages_exporter.py
@@ -9,8 +9,43 @@ from scripts.packages_exporter import Exporter
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") is not None
 
 
-@pytest.mark.skipif(
-    IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions."
+@pytest.mark.skip(
+    reason="""
+        This test needs to be rewritten.
+
+        First, it needs to be converted into an async test.
+
+        We need to bring up a configured sign_file instance when setting up the
+        test environment. This is, prepare everything to run:
+
+        `docker compose run --no-deps web_server db sign_file ...`
+
+        The sign_file service should:
+        * Have a user inside db
+        * A key in gpg database
+
+        Also, there is no point in getting the sign_keys from web_server as we
+        haven't loaded any keys into testing db. We should load sign keys into
+        db to request keys from web_server if we want this test to work that
+        way. Which can be easily done using a fixture.
+        In any case, I propose to, simply, pass the key_id of the key(s) that
+        we add into sign_file service when setting up the test environment and
+        skip the call to `exporter.get_sign_keys`, which can be tested
+        elsewhere, but not in this file.
+
+        Bear in mind that the exporter should know the sign_file user
+        credentials as they are used to request a token from sign_file
+        service.
+
+        pytest's tmp_path fixture can be of help to deal with tmp_files,
+        i.e.:
+        ```
+            def test_repomd_signer(tmp_path: Path):
+                tmp_file = tmp_path / "file.txt"
+                tmp_file.write_bytes(b"Hello world!")
+                ...
+        ```
+    """
 )
 def test_repomd_signer():
     exporter = Exporter(
