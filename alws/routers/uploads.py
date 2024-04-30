6b9155382d02dca559c2c3554346866b8bd27786
bklvsky <alexandrakachanova@gmail.com>
2024-04-30 15:24:45 +0000

    Getting product dynamically, enabling test_uploads

--- alws/routers/uploads.py
+++ alws/routers/uploads.py
@@ -2,12 +2,15 @@ import typing
 
 from fastapi import APIRouter, Depends, Form, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.orm import selectinload
+
 
 from alws.auth import get_current_user
 from alws.crud.products import get_products
 from alws.dependencies import get_db
 from alws.errors import PermissionDenied
-from alws.models import User
+from alws.models import User, Product, Repository, Build
 from alws.perms import actions
 from alws.perms.authorization import can_perform
 from alws.utils.uploader import MetadataUploader
@@ -18,23 +21,39 @@ router = APIRouter(
     dependencies=[Depends(get_current_user)],
 )
 
+async def get_repo_object(session: AsyncSession, repository):
+    products = (await session.execute(
+        select(Product).filter(
+            Product.repositories.any(Repository.name == repository)
+            )
+        )).scalars().all()
+    if not products:
+        products = []
+        builds = (await session.execute(
+            select(Build)
+            .filter(Build.repos.any(Repository.name == repository))
+            .options(selectinload(Build.products))
+            )).scalars().all()
+        for build in builds:
+            products.append(build.products)
+    return products
 
 @router.post("/upload_repometada/")
 async def upload_repometada(
     modules: typing.Optional[UploadFile] = None,
     comps: typing.Optional[UploadFile] = None,
     repository: str = Form(...),
-    product_name: str = Form(...),
     session: AsyncSession = Depends(get_db),
     user: User = Depends(get_current_user),
 ):
     msg = ""
     uploader = MetadataUploader(session, repository)
-    product = get_products(session, product_name=product_name)
-    if not can_perform(product, user, actions.ReleaseToProduct.name):
-        raise PermissionDenied(
-            "User does not have permissions to upload repository metadata."
-        )
+    repo_objects = await get_repo_object(session, repository)
+    for repo_obj in repo_objects:
+        if not can_perform(repo_obj, user, actions.ReleaseToProduct.name):
+            raise PermissionDenied(
+                "User does not have permissions to upload repository metadata to the repository."
+            )
     if modules is None and comps is None:
         return {"error": "there is nothing to upload"}
     updated_metadata = await uploader.process_uploaded_files(modules, comps)
