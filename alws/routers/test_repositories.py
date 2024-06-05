de952776d4e17b9018fed790ed7e011d088850c5
Aiden <aiden@boriska.com>
2024-06-05 20:29:05 +0000

    Commented out handlers for debugging

--- alws/routers/test_repositories.py
+++ alws/routers/test_repositories.py
@@ -77,14 +77,14 @@ async def create_repository(
     return created_repo
 
 
-@router.patch(
-    '/{repository_id}/',
-    status_code=status.HTTP_204_NO_CONTENT,
-)
+# @router.patch(
+#     '/{repository_id}/',
+#     status_code=status.HTTP_204_NO_CONTENT,
+# )
 async def update_test_repository(
     repository_id: int,
     payload: test_repository_schema.TestRepositoryUpdate,
-    session: AsyncSession = Depends(get_async_db_key()),
+    session: AsyncSessionDependency = Depends(get_async_db_key()),
     user: models.User = Depends(get_current_user),
 ):
     try:
@@ -101,13 +101,13 @@ async def update_test_repository(
         )
 
 
-@router.delete(
-    '/{repository_id}/remove/',
-    status_code=status.HTTP_204_NO_CONTENT,
-)
+# @router.delete(
+#     '/{repository_id}/remove/',
+#     status_code=status.HTTP_204_NO_CONTENT,
+# )
 async def remove_test_repository(
     repository_id: int,
-    session: AsyncSession = Depends(get_async_db_key()),
+    session: AsyncSessionDependency = Depends(get_async_db_key()),
     user: models.User = Depends(get_current_user),
 ):
     try:
